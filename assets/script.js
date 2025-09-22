86f2708b46fc9bc72ccce973134e659eb3160a06
stelladornelasbambui <140439856+stelladornelasbambui@users.noreply.github.com>
2025-09-22 20:11:06 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// ================== CONFIG ==================
+// ================== CONFIG ===================
 let CONFIG = {
     maxChars: 2000,
     sheetId: '1nT_ccRwFtEWiYvh5s4iyIDTgOj5heLnXSixropbGL8s',
